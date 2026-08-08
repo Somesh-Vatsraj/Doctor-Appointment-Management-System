14891139442347fa2e8dfc60d09c0a43cec5e5b1
Somesh-Vatsraj <someshsoftwareengineer@gmail.com>
2026-08-08 21:29:21 +0530

    Update text.php

--- text.php
+++ text.php
@@ -3,3 +3,10 @@
 echo extension_loaded('pdo_mysql')
     ? '✅ PDO MySQL is enabled'
     : '❌ PDO MySQL is NOT enabled';
+echo '<pre>';
+
+echo "PHP Version: " . PHP_VERSION . PHP_EOL;
+echo "PDO: " . (extension_loaded('pdo') ? 'YES' : 'NO') . PHP_EOL;
+echo "PDO MySQL: " . (extension_loaded('pdo_mysql') ? 'YES' : 'NO') . PHP_EOL;
+
+print_r(PDO::getAvailableDrivers());
